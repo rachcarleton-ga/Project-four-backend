fe467e1a38c54bdbe3f85e65ea862a9bca9241dd
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-09 13:05:02 -0800

    router update

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const morgan = require("morgan")
 const arrivedRouter = require('./routes/arrived');
 const goalRouter = require('./routes/goal')
 const journalRouter = require('./routes/journal')
+const AuthRouter = require('./routes/AuthRouter')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json()); 
@@ -24,6 +25,7 @@ app.use(morgan("dev"));
 app.use('/goal', goalRouter)
 app.use('/arrived', arrivedRouter)
 app.use('/journal', journalRouter)
+app.use('/auth', AuthRouter )
 
 app.get("/", (req, res) => {
     res.send("hello world");
