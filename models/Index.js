2d6e3759d82f5f3e24b9f87a6cd92042cb89cf60
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-09 11:13:32 -0800

    oauth backend

--- models/Index.js
+++ models/Index.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose')
+const userSchema = require('./User')
+const User = mongoose.model('User', userSchema)
+
+
 module.exports = {
     Arrived: require('./Arrived'),
     Goal: require('./Goal'),
--- models/User.js
+++ models/User.js
@@ -1,19 +1,13 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const UserSchema = new Schema ({
-    userName: {
-        type: String,
-        required: true,
+const UserSchema = new Schema (
+    {
+      name: { type: String, required: true },
+      email: { type: String, required: true },
+      passwordDigest: { type: String, required: true }
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    passwordDigest: {
-        type: String,
-        required: true,
-    },
-},{timestamps: true});
+    { timestamps: true }
+  )
 
 module.exports = mongoose.model("User", UserSchema)
