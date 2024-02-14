50112c25f585d8a30b984cf1cda1fcf5af878445
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-14 08:01:28 -0800

    journal edit routes

--- routes/journal.js
+++ routes/journal.js
@@ -6,11 +6,15 @@ router.post('/goal/:id', journalCtrl.goalCreate);
 
 router.get('/goal/:id', journalCtrl.goalShow);
 
+router.put('/goal/:id', journalCtrl.goalEdit);
+
 router.delete('/goal/:id', journalCtrl.goalDelete);
 
 router.post('/arrived/:id', journalCtrl.arrivedCreate);
 
 router.get('/arrived/:id', journalCtrl.arrivedShow);
+
+router.put('/arrived/:id', journalCtrl.arrivedEdit);
 
 router.delete('/arrived/:id', journalCtrl.arrivedDelete);
 
