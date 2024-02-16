e1ab80d58be09704a9cab4331eb8504ab5f70d4a
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-16 09:13:29 -0800

    goal user update

--- models/Goal.js
+++ models/Goal.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const GoalSchema = new Schema ({
+const GoalSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
     location: String,
     date: String,
     picture: String,
-}, {timestamps: true})
+}, { timestamps: true })
 
 module.exports = mongoose.model('Goal', GoalSchema)
